2f50f684888aa24b9a72a8ad259e3f25c46331d4
Scecil044 <scecil072@gmail.com>
2024-08-12 17:52:50 +0300

    changes on how messages are returned on errorhandler

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -3,8 +3,8 @@ const he = require("he");
 const errorHandler = (statusCode, message) => {
     const error = new Error();
     error.statusCode = statusCode;
-    // error.message = he.decode(message);
-    error.message = message.replace(/\\\"/g, '"'); 
+    error.message = he.decode(message);
+    error.message = message.replace(/(\"\s?)/g, '');
 
     return error;
 }
